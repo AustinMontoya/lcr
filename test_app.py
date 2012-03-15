df46ea09780a2af38e721bff68293d647d198d6f
unknown <alan.workman.ctr@adlnet.gov>
2012-03-15 12:51:25 -0400

    Added stubbed out test cases and wrote the code for some test cases

--- test_app.py
+++ test_app.py
@@ -32,34 +32,65 @@ class BaseTestCase(TestCase):
 
 class BaseAppTestCase(BaseTestCase):
 
-    testContent = {
-        'title' : 'testTitle',
-        'description' : 'Test description',
-        'tags' : ['tag1', 'tag2']
-    }
-
     def setup(self):
         self.app = create_app(TestConfig).test_client()
+        self.testContent = {
+            'title' : 'testTitle',
+            'description' : 'Test description',
+            'tags' : ['tag1', 'tag2']
+        }
+
     def teardown(self):
         for lcr in models.LearningObject.query.all():
             lcr.remove()
 
-    def test_content_create(self):
-        print json.dumps(self.testContent)
+    def test_content_create_success(self):
+        print 'test_content_create(self) Test Document: ' + str(json.dumps(self.testContent))
         response = self.app.post('/api/create/content', 
                         content_type='application/json',
                         data=json.dumps(self.testContent),
                         follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-
+        print 'test_content_create(self) status code: ' + str(response.status_code)
         vals = json.loads(response.data)
         self.assertEqual(vals['success'],True)
         self.assertIn('id', vals)
+
+    def test_content_create_fail(self): 
+        bad_content = self.testContent
+        bad_content['title'] = True
+        response = self.app.post('/api/create/content', 
+                        content_type='application/json',
+                        data=json.dumps(bad_content),
+                        follow_redirects=True)
+        self.assertGreaterEqual(response.status_code, 400)
+        vals = json.loads(response.data)
+        self.assertIn('error', vals)
+
 
     def test_content_retrieve(self):
         rv = self.app.get('/api/content/5')
         assert 'true' in rv.data
        # print rv.data
+
+
+    def test_content_update(self):
+        raise NotImplementedError("TODO")
+
+    def test_content_delete(self):
+        raise NotImplementedError("TODO")
+
+    def test_resource_create(self):
+        raise NotImplementedError("TODO")
+
+    def test_resource_retrieve(self):
+        raise NotImplementedError("TODO")
+
+    def test_resource_update(self):
+        raise NotImplementedError("TODO")
+
+    def test_resource_delete(self):
+        raise NotImplementedError("TODO")
 
 if __name__ == '__main__':
     unittest.main()        
